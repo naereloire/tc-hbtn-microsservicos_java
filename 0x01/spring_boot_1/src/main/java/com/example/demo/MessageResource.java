42b4b2c12c0afd2c7bd588831437f97acb21dd21
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-06-20 20:12:26 -0300

    init

--- 0x01/spring_boot_1/src/main/java/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/src/main/java/com/example/demo/MessageResource.java
@@ -13,4 +13,18 @@ public class MessageResource {
 
     return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
   }
+
+  @GetMapping("/login")
+  public String login(String user, String password) {
+    String result = null;
+    if (user.isEmpty() || password == null) {
+      result = "USUÁRIO E SENHA NÃO INFORMADOS";
+    } else if (user.length() > 15 || password == null || password.length() > 15) {
+      result = "USUÁRIO E SENHA INVÁLIDOS";
+    } else {
+      result = "LOGIN EFETUADO COM SUCESSO !!!";
+    }
+
+    return result;
+  }
 }
