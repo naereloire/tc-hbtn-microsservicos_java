639d303aa2f2f9f60961443e94b387dc6d9d59a6
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-06-27 17:35:52 -0300

    [mega-sena-api] fix dir structure

--- 0x05/MegaSenaAPI/src/main/java/com/song/MegaSenaAPI/MegaSenaController.java
+++ 0x05/MegaSenaAPI/src/main/java/com/song/MegaSenaAPI/MegaSenaController.java
@@ -4,23 +4,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @RestController
-@RequestMapping(value="/megasena")
+@RequestMapping(value = "/megasena")
 public class MegaSenaController {
 
-    @GetMapping("/simpleMessageWelcome")
-    public String mensagemBoasVindas() {
-       String message = "Bem vindo a API REST para geração de números para a loteria Mega Sena";
-        return message;
+  @GetMapping("/simpleMessageWelcome")
+  public String mensagemBoasVindas() {
+    String message = "Bem vindo a API REST para geração de números para a loteria Mega Sena";
+    return message;
+  }
+
+  @GetMapping("/getNumbers")
+  public List<Integer> numerosMegaSena() {
+    List<Integer> list = new ArrayList<>();
+    Random rand = new Random();
+    int upperbound = 7;
+    int int_random;
+
+    for (int i = 1; i < upperbound; i++) {
+      int_random = rand.nextInt(upperbound);
+      list.add(int_random);
     }
 
-
-    @GetMapping("/getNumbers")
-    public List<Integer> numerosMegaSena() {
-        // TODO
-        // Obs: Retornar os números gerados em ordem crescente.
-        return null;
-    }
+    return list;
+  }
 }
