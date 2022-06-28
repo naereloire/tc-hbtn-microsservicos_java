bb56692a9bd5a35268d0500a3ce2caae94eb0f3e
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-06-28 17:15:10 -0300

    [calculator-api] add structure

--- 0x07/api-rest-calculator/src/test/java/com/example/calculator/controller/CalculatorControllerTest.java
+++ 0x07/api-rest-calculator/src/test/java/com/example/calculator/controller/CalculatorControllerTest.java
@@ -18,12 +18,12 @@ class CalculatorControllerTest {
 
   @Autowired private MockMvc mvc;
 
-  @Test
-  void messageWelcome() throws Exception {
-    RequestBuilder request = get("/calculator/welcome");
-    MvcResult result = mvc.perform(request).andReturn();
-    assertEquals("Bem vindo à CALCULATOR API REST.", result.getResponse().getContentAsString());
-  }
+//  @Test
+//  void messageWelcome() throws Exception {
+//    RequestBuilder request = get("/calculator/welcome");
+//    MvcResult result = mvc.perform(request).andReturn();
+//    assertEquals("Bem vindo à CALCULATOR API REST.", result.getResponse().getContentAsString());
+//  }
 
   @Test
   void addNumbers() throws Exception {
