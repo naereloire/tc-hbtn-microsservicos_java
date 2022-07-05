6deff991dc67d67d9b3a17558b29a9746ea0abc8
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-07-05 19:14:48 -0300

    [swagger] fix config to change description into swagger

--- 0x08/ProductAPI/src/main/java/com/example/product/config/SwaggerConfiguration.java
+++ 0x08/ProductAPI/src/main/java/com/example/product/config/SwaggerConfiguration.java
@@ -2,8 +2,12 @@ package com.example.product.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -13,13 +17,13 @@ public class SwaggerConfiguration extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket docket() {
-        // TODO Implementar
-        return null;
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).build();
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // TODO Implementar
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
 
