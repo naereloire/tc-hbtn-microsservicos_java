f21fc4bb49e74936dbf230ba450b2269ce8166b5
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-07-05 17:43:35 -0300

    [swagger] add crud methds

--- 0x08/ProductAPI/src/main/java/com/example/product/model/ProductRepository.java
+++ 0x08/ProductAPI/src/main/java/com/example/product/model/ProductRepository.java
@@ -8,31 +8,41 @@ import java.util.List;
 @Repository
 public class ProductRepository {
 
-    private final List<Product> list = new ArrayList<Product>();
+    private final List<Product> list = new ArrayList<>();
 
     public List<Product> getAllProducts() {
-        // TODO Implementar
-        return null;
+        return list;
     }
 
     public Product getProductById(Integer id) {
-        // TODO Implementar
-        return null;
+        return list.stream().filter(product -> product.getId().equals(id)).findAny().orElse(null);
     }
 
     public void addProduct(Product s) {
-        // TODO Implementar
+        if (!list.contains(s)) {
+            list.add(s);
+        }
     }
 
     public void updateProduct(Product s) {
-        // TODO Implementar
+        Product productId = getProductById(Math.toIntExact(s.getId()));
+        if (list.contains(s)) {
+            int index = list.indexOf(productId);
+            list.set(index, s);
+        }
     }
 
     public void removeProduct(Product s) {
-        // TODO Implementar
+        Product productId = getProductById(Math.toIntExact(s.getId()));
+        if (list.contains(s)) {
+            list.remove(productId);
+        }
     }
 
     public void addList(List<Product> listOfProducts) {
-        // TODO Implementar
+        List<List<Product>> newlist = new ArrayList<>();
+        if (!newlist.contains(listOfProducts)) {
+            newlist.add(listOfProducts);
+        }
     }
 }
